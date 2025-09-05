7eff66a2df0689002c10b3896c65785bbc2c9530
Tanjimul Islam <tanjimul.islam@hotmail.com>
2025-09-05 16:41:10 +0600

    chore: bump version to 0.1.0-alpha.4

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name("react-project-kit")
   .description("Create a new React project with modern tooling")
-  .version("1.0.0")
+  .version("0.1.0-alpha.4")
   .argument("[project-name]", "Name of the project")
   .action(async (projectName) => {
     await createProject(projectName);
